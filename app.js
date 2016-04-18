9a3908ad0e9f30e930fb69dc5701706591eb2150
Churro <srilankanchurro@gmail.com>
2016-04-17 22:17:54 -0700

    Simple sockets set up

--- app.js
+++ app.js
@@ -5,25 +5,28 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var io = require('socket.io');
+/*var routes = require('./routes/index');
+var users = require('./routes/users');*/
+var api = require('./api');
 var app = express();
 
 // view engine setup
-/*app.set('views', path.join(__dirname, 'views'));
- app.set('view engine', 'jade');*/
+// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use("/api", api);
+
+/*app.use('/', routes);
+app.use('/users', users);*/
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -54,6 +57,62 @@ app.use(function (err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+
+
+
+var server = require('http').Server(app);
+
+
+var waitingSession;
+/**
+ * Socket.io
+ */
+var io = require('socket.io')(server);
+
+io.on('connection', function (socket) {
+});
+
+io.on('connection', function (socket) {
+  var hrTime = process.hrtime();
+  var sessionId = (hrTime[0] * 1000000 + hrTime[1] / 1000).toString();
+  console.log("A user connected. Session ID is", sessionId);
+  socket.emit("session", sessionId);
+
+  console.log("Rooms", socket.rooms);
+
+  if (waitingSession) {
+    socket.join(waitingSession, function(err) {
+      if (err) {
+        console.log("Error joining room", waitingSession, err);
+      }
+      else {
+        console.log("Successfully joined client to", waitingSession);
+      }
+    });
+
+    console.log("Connecting client", sessionId, "with", waitingSession);
+    socket.emit("partner", {partner: waitingSession});
+    waitingSession = null;
+  }
+  else {
+    socket.emit("partner", {});
+    waitingSession = sessionId;
+  }
+
+  
+
+  /*socket.emit('news', { hello: 'world' });
+   socket.on('my other event', function (data) {
+   console.log(data);
+   });*/
+});
+
+var port = process.env.PORT || '8000';
+
+
+server.listen(port, function(){
+  console.log('listening on', server.address());
 });
 
 
